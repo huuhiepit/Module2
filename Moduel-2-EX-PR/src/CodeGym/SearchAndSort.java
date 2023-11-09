53edb9d54795bc4f78d5f60cf903142b526938e1
Nguyen Huu Hiep <nhhiepit@gmail.com>
2023-11-09 09:00:42 +0700

    Date 11/09/2023

--- Moduel-2-EX-PR/src/CodeGym/SearchAndSort.java
+++ Moduel-2-EX-PR/src/CodeGym/SearchAndSort.java
@@ -2,6 +2,12 @@ package CodeGym;
 
 public class SearchAndSort {
     public static void main(String[] args) {
+		//Binary search
+		int[] nums01 = {2, 5, 8, 12, 16, 23, 38, 56, 72, 91};
+		//System.out.println(binarySearch(nums01, 23);
+		//Linear Search
+		int[] nums02 = {2, 3, 5, 9, 10, 11, 15, 20, 21, 30};
+		//System.out.println(linearSearch(nums02, 15);
         int[] nums = {1,3,4,6,7,9};
         System.out.println(binarySearch(nums, 4));
     }
@@ -70,13 +76,25 @@ public class SearchAndSort {
 
     public static void interchangeSort(int[] nums) {
         for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = 0; j < nums.length - i - 1; j++) {
-                if (nums[j] > nums[j + 1]) {
-                    int temp = nums[j];
-                    nums[j] = nums[j + 1];
-                    nums[j + 1] = temp;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] > nums[j]) {
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
                 }
             }
+        }
+    }
+	public static void insertionSort(int[] nums) {
+        for(int i = 1; i < nums.length; i++) {
+            int currentNum = nums[i];
+            int j = i - 1;
+            while(j >= 0 && nums[j] > currentNum) {
+                nums[j + 1] = nums[j];
+                j--;
+            }
+            
+            nums[j + 1] = currentNum;
         }
     }
 }
